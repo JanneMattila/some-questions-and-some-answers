bde6386f0ad63fc54450e8f461ab94fa4feb3e9c
Janne Mattila <janne.mattila@hotmail.fi>
2021-05-07 13:17:47 +0300

    Update to send multiple messages

--- q&a/la_and_servicebus.ps1
+++ q&a/la_and_servicebus.ps1
@@ -1,30 +1,38 @@
+
+# Relevant links:
+# https://docs.microsoft.com/en-us/rest/api/servicebus/send-message-to-queue
+
 $serviceBusNamespace = "sb2lademo"
-$queue = "no-sessions"
+$queue = "q"
 
 $url = "https://$serviceBusNamespace.servicebus.windows.net/$queue/messages?api-version=2014-01"
 $url
 
 $accessToken = ConvertTo-SecureString -AsPlainText -String (az account get-access-token --resource https://servicebus.azure.net --query accessToken -o TSV)
-$body = ConvertTo-Json @{
-    "specversion"     = "1.0"
-    "type"            = "ERP.Sales.Order.Created"
-    "source"          = "/mycontext"
-    "subject"         = $null
-    "id"              = "C1234-1234-1234"
-    "time"            = [System.DateTime]::UtcNow
-    "datacontenttype" = "application/json"
-    "data"            = @{
-        "appinfoA" = "abc"
-        "appinfoB" = 123
-        "appinfoC" = $true
-    }
-}
-$body
 
-Invoke-RestMethod `
-    -Body $body `
-    -ContentType "application/atom+xml;type=entry;charset=utf-8" `
-    -Method "POST" `
-    -Authentication Bearer `
-    -Token $accessToken `
-    -Uri $url
+$count = 100
+for ($i = 0; $i -lt $count; $i++) {
+    Write-Progress -Activity "Send messages" -Status "$i / $count" -PercentComplete ($i/$count*100)
+    $body = ConvertTo-Json @{
+        "specversion"     = "1.0"
+        "type"            = "ERP.Sales.Order.Created"
+        "source"          = "/mycontext"
+        "subject"         = $null
+        "id"              = "C1234-1234-1234"
+        "time"            = [System.DateTime]::UtcNow
+        "datacontenttype" = "application/json"
+        "data"            = @{
+            "appinfoA" = "abc"
+            "appinfoB" = 1
+            "appinfoC" = $true
+        }
+    }
+
+    Invoke-RestMethod `
+        -Body $body `
+        -ContentType "application/atom+xml;type=entry;charset=utf-8" `
+        -Method "POST" `
+        -Authentication Bearer `
+        -Token $accessToken `
+        -Uri $url    
+}
