4065ba583625cddc8db64311f2a464a014089a95
Janne Mattila <janne.mattila@hotmail.fi>
2023-10-06 11:03:11 +0300

    Add AAD Group synchonization example

---
+++ q&a/aad-sync-groups.ps1
@@ -0,0 +1,83 @@
+Param (
+    [Parameter(HelpMessage = "Source Azure AD Group Object ID")]
+    [string] $SourceGroupID,
+
+    [Parameter(HelpMessage = "Target Azure AD Group Object ID")]
+    [string] $TargetGroupID
+)
+
+$ErrorActionPreference = "Stop"
+
+$token = (Get-AzAccessToken -ResourceUrl "https://graph.microsoft.com/").Token
+$bearerToken = ConvertTo-SecureString -String $token -AsPlainText
+
+function Get-AllMembers($Collection, $ObjectID) {
+    "Getting members for group $ObjectID"
+    $groupJson = Invoke-RestMethod -Uri "https://graph.microsoft.com/v1.0/groups/$ObjectID/members" -Authentication Bearer -Token $bearerToken
+
+    foreach ($member in $groupJson.value) {
+        "Processing member $($member.displayName), type: $($member.'@odata.type')"
+
+        if ($member.'@odata.type' -eq "#microsoft.graph.group") {
+            "Group $($member.displayName) found"
+            Get-AllMembers $Collection $member.id
+        }
+        elseif ($member.'@odata.type' -eq "#microsoft.graph.user") {
+            if ($Collection.ContainsKey($member.id) -eq $true) {
+                "User $($member.displayName) already exists in collection"
+            }
+            else {
+                "User $($member.displayName) added to collection"
+                $Collection.Add($member.id, $member) | Out-Null
+            }
+        }
+    }
+}
+
+$sourceUsers = @{}
+Get-AllMembers $sourceUsers $SourceGroupID
+$sourceUsers
+
+$targetUsers = @{}
+Get-AllMembers $targetUsers $TargetGroupID
+$targetUsers
+
+$usersToAdd = @{}
+$usersToRemove = @{}
+
+foreach ($sourceUser in $sourceUsers.Values) {
+    if ($targetUsers.ContainsKey($sourceUser.id) -eq $false) {
+        "User $($sourceUser.displayName) should be added to target group"
+        $usersToAdd.Add($sourceUser.id, $sourceUser) | Out-Null
+    }
+}
+
+foreach ($targetUser in $targetUsers.Values) {
+    if ($sourceUsers.ContainsKey($targetUser.id) -eq $false) {
+        "User $($targetUser.displayName) should be removed from target group"
+        $usersToRemove.Add($targetUser.id, $targetUser) | Out-Null
+    }
+}
+
+$usersToAdd
+$usersToRemove
+
+$usersToAdd.Values | ForEach-Object {
+    $user = $_
+    $body = @{}
+    $body.Add("@odata.id", "https://graph.microsoft.com/v1.0/directoryObjects/$($user.id)")
+    $bodyJson = $body | ConvertTo-Json
+
+    "Adding user $($user.displayName) to target group"
+    Invoke-RestMethod -Uri "https://graph.microsoft.com/v1.0/groups/$TargetGroupID/members/`$ref" -Method POST -Body $bodyJson -ContentType "application/json" -Authentication Bearer -Token $bearerToken
+}
+
+$usersToRemove.Values | ForEach-Object {
+    $user = $_
+    $body = @{}
+    $body.Add("@odata.id", "https://graph.microsoft.com/v1.0/users/$($user.id)")
+    $bodyJson = $body | ConvertTo-Json
+
+    "Removing user $($user.displayName) from target group"
+    Invoke-RestMethod -Uri "https://graph.microsoft.com/v1.0/groups/$TargetGroupID/members/$($user.id)/`$ref" -Method DELETE -Body $bodyJson -ContentType "application/json" -Authentication Bearer -Token $bearerToken
+}
